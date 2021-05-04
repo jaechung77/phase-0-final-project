3467e7eea6483a128571127c568ef0a3c71be894
jaechung77 <jaechung77@gmail.com>
2021-05-05 09:10:29 +1000

    FIrst Ass1

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 const toggleList = document.querySelector(".toggleList");
-const countries = document.querySelector("#countries");
+const countries = document.querySelector(".countries");
 toggleList.addEventListener("click", function(){
-countries.classList.toggle("active");
+    countries.classList.toggle("active");
 });
