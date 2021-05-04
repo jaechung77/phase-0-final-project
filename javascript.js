2aef882e174c36c8933ee09d0cb7532c6cc7d0b5
jaechung77 <jaechung77@gmail.com>
2021-05-05 08:44:25 +1000

    Assignment 1

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const toggleList = document.querySelector(".toggleList");
+const countries = document.querySelector("#countries");
+toggleList.addEventListener("click", function(){
+countries.classList.toggle("active");
+});
