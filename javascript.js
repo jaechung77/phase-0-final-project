8d9d949623373d20dcc5218af56053779c3d111d
jaechung77 <jaechung77@gmail.com>
2021-05-05 20:43:38 +1000

    So hare

--- javascript.js
+++ javascript.js
@@ -1,5 +1,23 @@
 const toggleList = document.querySelector(".toggleList");
-const countries = document.querySelector(".countries");
+const destinations = document.querySelector(".destinations");
+let country = document.querySelectorAll('input[name="country"');
+const submit = document.querySelector("#submit");
+let selected = false;
+
 toggleList.addEventListener("click", function(){
-    countries.classList.toggle("active");
+    destinations.classList.toggle("active");
+    
 });
+
+
+for (let i=0; i < country.length; i++){
+    country[i].addEventListener("change", function(){
+        selected= true;
+        console.log("Inside Loop: " + selected);
+        //submit.classList.toggle("active"); 
+    });
+}
+console.log("Outside of Loop: " + selected);
+if (selected){
+     submit.classList.toggle("active");    
+}  
